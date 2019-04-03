630aaff76319fdee8625978419cc0b51a7ea8851
David J. Brenes <davidjbrenes@gmail.com>
2019-04-03 20:41:59 +0200

    Changing configuration to env vars for Heroku

--- twitter-purge.rb
+++ twitter-purge.rb
@@ -4,13 +4,12 @@ require 'date'
 require 'rubygems'
 require 'twitter'
 
-USERNAME            = '<username>'            # Your username without the `@`
-CONSUMER_KEY        = '<consumer-key>'        # Consumer key from Twitter
-CONSUMER_SECRET     = '<consumer-secret>'     # Consumer secret from Twitter
-ACCESS_TOKEN        = '<access-token>'        # Access token from Twitter
-ACCESS_TOKEN_SECRET = '<access-token-secret>' # Access token secret from Twitter
-DAYS_TO_KEEP        = 7                       # How many days to keep tweets?
-ATTEMPTS_LIMIT      = 1                       # How many times do we try this?
+USERNAME            = ENV['USERNAME']
+CONSUMER_KEY        = ENV['CONSUMER_KEY']
+CONSUMER_SECRET     = ENV['CONSUMER_SECRET']
+ACCESS_TOKEN        = ENV['ACCESS_TOKEN']
+ACCESS_TOKEN_SECRET = ENV['ACCESS_TOKEN_SECRET']
+DAYS_TO_KEEP        = ENV['DAYS_TO_KEEP'].to_i
 
 client = Twitter::REST::Client.new({
   :consumer_key        => CONSUMER_KEY,
